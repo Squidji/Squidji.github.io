dd9f4edd5efe51d13aa454dbfa4461565654a9e6
Ben Squidji <sluggyben@hotmail.com>
2019-12-07 12:40:02 -0500

    PETRICHOR v1.5.0

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let pagehistory = {
 }
 
 function openPage(pagename) {
+	pagehistory.Previous = pagehistory.Current;
+	pagehistory.Current = pagename;
 	let allpages = document.getElementsByClassName('page');
 	for (let i=0; i<allpages.length; i++) {
 		allpages[i].style.display = 'none';
 	};
 	window.scrollTo(0,0);
-	pagehistory.Previous = pagehistory.Current;
-	pagehistory.Current = pagename;
 	document.getElementById('Start').style.display = 'block';
 	document.getElementById('Tabs').style.display = 'block';
 	document.getElementById(pagename).style.display = 'block';
