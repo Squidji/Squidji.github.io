791299e0cb83d536296d749d1811bfafbca279e0
Ben Squidji <sluggyben@hotmail.com>
2020-02-16 01:23:29 -0500

    liquify v2 [Beta]

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ function openTab(tabname) {
 	for (let i=0; i<alltabs.length; i++) {
 		alltabs[i].setAttribute('f', 'n');
 	};
+	alltabs = document.getElementById('NavTabs').children;
+	for (let i=0; i<alltabs.length; i++) {
+		alltabs[i].setAttribute('f', 'n');
+	};
 	document.getElementById('Tab_' + tabname).setAttribute('f', 'y');
+	document.getElementById('Nav_' + tabname).setAttribute('f', 'y');
 	alltabs = document.getElementsByClassName('tabpage');
 	for (let i=0; i<alltabs.length; i++) {
 		alltabs[i].style.display = 'none';
 	};
 	document.getElementById('Page_' + tabname).style.display = 'block';
+	closeNav();
 }
 
 function openWork(workname) {
@@ -25,14 +31,16 @@ function openLog(i) {
 	document.getElementById('ShortLogs_H2').innerHTML = 'Log ' + i + ' - ' + logData[i].date;
 	document.getElementById('ShortLogs_P').innerText = logData[i].text;
 
-	let alltabs
-	alltabs = document.getElementById('Tabs').children;
-	for (let i=0; i<alltabs.length; i++) {
-		alltabs[i].setAttribute('f', 'n');
-	};
-	alltabs = document.getElementsByClassName('tabpage');
+	let alltabs = document.getElementsByClassName('tabpage');
 	for (let i=0; i<alltabs.length; i++) {
 		alltabs[i].style.display = 'none';
 	};
 	document.getElementById('Page_ShortLogs').style.display = 'block';
+}
+
+function openNav() {
+	document.getElementById('Nav').style.width = '100%';
+}
+function closeNav() {
+	document.getElementById('Nav').style.width = '0';
 }
