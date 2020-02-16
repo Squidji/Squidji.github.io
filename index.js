3cf6c8b76f94200d44ba0c6594c749c670fb10c9
Ben Squidji <sluggyben@hotmail.com>
2020-02-15 23:47:51 -0500

    liquify v2 [Beta]
    New design, new projects,

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
-let pagehistory = {
-	'Current': undefined,
-	'Previous': undefined,
-	'Log': undefined
+function openTab(tabname) {
+	let alltabs
+	alltabs = document.getElementById('Tabs').children;
+	for (let i=0; i<alltabs.length; i++) {
+		alltabs[i].setAttribute('f', 'n');
+	};
+	document.getElementById('Tab_' + tabname).setAttribute('f', 'y');
+	alltabs = document.getElementsByClassName('tabpage');
+	for (let i=0; i<alltabs.length; i++) {
+		alltabs[i].style.display = 'none';
+	};
+	document.getElementById('Page_' + tabname).style.display = 'block';
 }
 
-function openPage(pagename) {
-	pagehistory.Previous = pagehistory.Current;
-	pagehistory.Current = pagename;
-	let allpages = document.getElementsByClassName('page');
-	for (let i=0; i<allpages.length; i++) {
-		allpages[i].style.display = 'none';
+function openWork(workname) {
+	let allworks = document.getElementsByClassName('workp');
+	for (let i=0; i<allworks.length; i++) {
+		allworks[i].style.display = 'none';
+	}
+	document.getElementById(workname).style.display = 'block';
+}
+
+function openLog(i) {
+	document.getElementById('ShortLogs_H1').innerHTML = logData[i].title;
+	document.getElementById('ShortLogs_H2').innerHTML = 'Log ' + i + ' - ' + logData[i].date;
+	document.getElementById('ShortLogs_P').innerText = logData[i].text;
+
+	let alltabs
+	alltabs = document.getElementById('Tabs').children;
+	for (let i=0; i<alltabs.length; i++) {
+		alltabs[i].setAttribute('f', 'n');
 	};
-	window.scrollTo(0,0);
-	document.getElementById('Start').style.display = 'block';
-	document.getElementById('Tabs').style.display = 'block';
-	document.getElementById(pagename).style.display = 'block';
-};
-
-function openLog(logid) {
-	openPage('Log');
-	pagehistory.Log = logid;
-	document.getElementById('logTitle').innerText = logData[logid].title;
-	document.getElementById('logDate').innerText = 'Log ' + 
-	logid + ' - ' + logData[logid].date;
-	document.getElementById('logBody').innerText = logData[logid].text;
-	if (logid > 0) {
-		document.getElementById('logPrev').innerHTML = '<i class="fas fa-angle-double-left"></i>'
-		 + 'Log ' + (logid - 1) + ' - ' + logData[logid - 1].title;
-		 document.getElementById('logPrev').onclick = prevlog;
-	} else {
-		document.getElementById('logPrev').innerHTML = 'No Previous Logs';
-		document.getElementById('logPrev').onclick = null;
-	}
-	if (logid !== Math.max(...Object.keys(logData)) ) {
-		document.getElementById('logNext').innerHTML = 'Log ' + (logid + 1) + ' - ' + logData[logid + 1].title +
-		'<i class="fas fa-angle-double-right"></i>';
-		document.getElementById('logNext').onclick = nextlog;
-	} else {
-		document.getElementById('logNext').innerHTML = 'No Next Logs';
-		document.getElementById('logNext').onclick = null;
-	}
-};
-
-function nextlog() {
-	let buffer = pagehistory.Previous;
-	openLog(pagehistory.Log + 1);
-	pagehistory.Previous = buffer;
-};
-function prevlog() {
-	let buffer = pagehistory.Previous;
-	openLog(pagehistory.Log - 1);
-	pagehistory.Previous = buffer;
-};
+	alltabs = document.getElementsByClassName('tabpage');
+	for (let i=0; i<alltabs.length; i++) {
+		alltabs[i].style.display = 'none';
+	};
+	document.getElementById('Page_ShortLogs').style.display = 'block';
+}
